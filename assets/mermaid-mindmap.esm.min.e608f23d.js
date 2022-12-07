54e792ba0abac56f4edaf06502dd9b22605df2c9
meta-docs <fangtaog@gmail.com>
2022-12-07 04:47:38 +0000

    Deploying to gh-pages from @ meta-docs/meta-docs.github.io@55364314f5e2fc7cafad0f85270757723466d0ce 🚀

--- assets/mermaid-mindmap.esm.min.e608f23d.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./app.015ebbe1.js";import"./framework.46148295.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.071fd575.6c978a03.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
